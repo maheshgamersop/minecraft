a1ecb363cbc1b51ccbaf3ecf13bf871e4c9764ba
maheshgamersop <141534592+maheshgamersop@users.noreply.github.com>
2024-07-29 22:01:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Set up CORS
-const allowedOrigins = ['https://front-add6.onrender.com', 'https://6be12338-8441-409d-8878-f7bee20af9de-00-ahmtmo5negz0.pike.replit.dev'];
+const allowedOrigins = ['https://backend-yv3h.onrender.com', 'https://6be12338-8441-409d-8878-f7bee20af9de-00-ahmtmo5negz0.pike.replit.dev'];
 
 app.use(cors({
   origin: function (origin, callback) {
