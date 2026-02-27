8ea3ed63ba7e65cf60bc67036d178c77828e663f
Mahesh Sharma <141534592+maheshgamersop@users.noreply.github.com>
2026-02-27 16:25:18 +0530

    Update mongodb.js

--- connection/mongodb.js
+++ connection/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connect(){
-  await mongoose.connect('mongodb+srv://thegangstaguy001:NuLcOmlDKV6UGNoi@cluster0.nh1ewxi.mongodb.net/minecraft?retryWrites=true&w=majority')
+  await mongoose.connect('mongodb+srv://thegangstaguy001:bmDlKbFBvWIKPvMc@cluster0.nh1ewxi.mongodb.net/minecraft?retryWrites=true&w=majority')
   .then((c)=>{
     console.log('Connected to MongoDB')
   })
