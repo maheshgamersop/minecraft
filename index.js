32c098fc95e52dc3a1163f809273f7d637fe2071
Mahesh Sharma <141534592+maheshgamersop@users.noreply.github.com>
2026-02-28 08:33:20 +0530

    Add new allowed origin for CORS

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Set up CORS
-const allowedOrigins = ['https://minecraft-yfrb.onrender.com','https://pay.nitrogen.kesug.com' ,'https://6be12338-8441-409d-8878-f7bee20af9de-00-ahmtmo5negz0.pike.replit.dev'];
+const allowedOrigins = ['https://block-buddy-works.lovable.app','https://minecraft-yfrb.onrender.com','https://pay.nitrogen.kesug.com' ,'https://6be12338-8441-409d-8878-f7bee20af9de-00-ahmtmo5negz0.pike.replit.dev'];
 
 app.use(cors({
   origin: function (origin, callback) {
